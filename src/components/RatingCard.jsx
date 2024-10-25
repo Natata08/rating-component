f23a0ac70a3e283ec90f052e1e3185d5d456116c
Natalia Lapina <lapinann08@gmail.com>
2024-10-25 15:09:26 +0200

    add styling for RatingCard component

--- src/components/RatingCard.jsx
+++ src/components/RatingCard.jsx
@@ -1,27 +1,36 @@
+import styles from "./RatingCard.module.css";
+
 const RatingCard = () => {
   const ratings = [1, 2, 3, 4, 5];
   return (
-    <div>
-      <div>
+    <div className={styles.card}>
+      <div className={styles.starIcon}>
         <img src='/images/icon-star.svg' alt='Star icon' />
       </div>
-      <h2>How did we do?</h2>
-      <p>
+      <h2 className={styles.title}>How did we do?</h2>
+      <p className={styles.description}>
         Please let us know how we did with your support request. All feedback is
         appreciated to help us improve our offering!
       </p>
       <form>
-        <fieldset>
-          <legend>Rate your experience</legend>
+        <fieldset className={styles.ratingFieldset}>
+          <legend className='srOnly'>Rate your experience</legend>
           {ratings.map((rating) => (
-            <label key={`rating-${rating}`}>
-              <input type='radio' name='rating' value={rating} />
-              <span>{rating}</span>
+            <label key={`rating-${rating}`} className={styles.ratingLabel}>
+              <input
+                type='radio'
+                name='rating'
+                value={rating}
+                className={styles.ratingInput}
+              />
+              <span className={styles.ratingButton}>{rating}</span>
             </label>
           ))}
         </fieldset>
 
-        <button type='submit'>Submit</button>
+        <button type='submit' className={styles.submitButton}>
+          Submit
+        </button>
       </form>
     </div>
   );
